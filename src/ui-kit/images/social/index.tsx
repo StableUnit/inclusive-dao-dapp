abbc6ed679651b61857a98709d040471cc8ea40e
Дмитрий <mitya-kudr@mail.ru>
2023-10-05 11:54:28 +0300

    maintenance branch

--- src/ui-kit/images/social/index.tsx
+++
@@ -1,5 +0,0 @@
-export { ReactComponent as TgIcon } from "./tg.svg";
-export { ReactComponent as DiscordIcon } from "./discord.svg";
-export { ReactComponent as MediumIcon } from "./medium.svg";
-export { ReactComponent as TwitterIcon } from "./twitter.svg";
-export { ReactComponent as GithubIcon } from "./github.svg";
